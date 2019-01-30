7372e2428d4a2926130985604c27e0e8cab3f0c9
santaclaushg@gmail.com <santaclaushg@gmail.com>
2019-01-30 14:19:12 +0700

    testt

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const _ = require('lodash');
+
+const app = express();
+
+//Routes
+const packageJson = require('./package.json');
+//Root
+app.get('/', (req, res) =>
+{
+    res.json(
+        _.pick(packageJson, ['name', 'version', 'description', 'author', 'license'])
+    )
+});
+
+const port = process.env.PORT || 9999;
+app.listen(port);
